4c061a4f3785820c1f2274369084afa28f0e1733
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2020-12-16 23:58:40 +0800

    added logo and fonts, and set background

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Footer />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="footer-wrapper">
+        <center><div class="logo-wrapper">
+            <img src="@/assets/suslogo.png">
+        </div></center>
+        hi
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+@font-face {
+  font-family: "Objectivity-Bold";
+  src: url('./../fonts/Objectivity-Bold.otf') format("opentype");
+}
+*{
+    font-family: "Objectivity-Bold";
+}
+.footer-wrapper{
+    background-color: #f8f8f8;
+    color: black;
+    width: 100%;
+    height: 400px;
+    overflow: hidden;
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+}
+.logo-wrapper{
+    padding-top: 5%;
+    width: 285px;
+    height: 72px;
+}
+.logo-wrapper img{
+    width: 100%;
+    height: 100%;
+}
+</style>
