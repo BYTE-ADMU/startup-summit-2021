7a6eee9cc8dffb1f89074f241ed63cf8a122539f
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2020-12-17 11:54:07 +0800

    added fonts and register button

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,12 @@
         <center><div class="logo-wrapper">
             <img src="@/assets/suslogo.png">
         </div></center>
-        hi
+        <div class="footer-menu">
+            <a href="#">Home</a>
+        </div>
+        <div class="register-btn">
+            <button>Register</button>
+        </div>
     </div>
 </template>
 
@@ -19,8 +24,16 @@ export default {
   font-family: "Objectivity-Bold";
   src: url('./../fonts/Objectivity-Bold.otf') format("opentype");
 }
+@font-face {
+  font-family: "Inter-Bold";
+  src: url('./../fonts/Inter-Bold.ttf') format("truetype");
+}
+@font-face {
+  font-family: "Inter-SemiBold";
+  src: url('./../fonts/Inter-SemiBold.ttf') format("truetype");
+}
 *{
-    font-family: "Objectivity-Bold";
+    font-family: 'Inter', sans-serif;
 }
 .footer-wrapper{
     background-color: #f8f8f8;
@@ -40,5 +53,30 @@ export default {
 .logo-wrapper img{
     width: 100%;
     height: 100%;
+}
+.footer-menu{
+    padding-top: 32px;
+}
+.footer-menu a{
+    text-decoration: none;
+    color: #333333!important;
+    font-family: 'Inter-Bold';
+}
+.register-btn{
+    padding-top: 32px;
+}
+.register-btn button{
+    background-color: #F46E26;
+    color: white;
+    padding: 8px 24px;
+    font-size: 16px;
+    line-height: 26px;
+    border: 0px;
+    border-radius:4px!important;
+    box-shadow: black!important;
+    box-shadow: 50px black;
+    cursor: pointer;
+    box-shadow: 0px 0px 20px rgba(244,110,38, 0.8);
+    font-family: 'Inter-SemiBold';
 }
 </style>
