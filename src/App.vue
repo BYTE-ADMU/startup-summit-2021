204fbd729e41b57937c381ab41166e10cfabd37a
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2020-12-18 19:18:24 +0800

    added fonts and assets and finished hero

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Hero />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/Hero'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hero
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0px;
 }
 </style>
