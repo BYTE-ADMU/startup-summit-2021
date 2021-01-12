5c098d4b4a6f0987739c74748f0e327d258e3b68
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2021-01-13 01:23:14 +0800

    added primary button component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PrimaryBtn>Register</PrimaryBtn>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PrimaryBtn from './components/PrimaryBtn.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PrimaryBtn
   }
 }
 </script>
---
+++ src/components/PrimaryBtn.vue
@@ -0,0 +1,38 @@
+<template>
+<button class="primary">
+  <slot>Button</slot>
+</button>
+</template>
+
+<script>
+export default {
+    name: 'Primary-Button'
+}
+</script>
+
+<style scoped>
+.primary{
+    background-color: #F46E26;
+    color: white;
+    padding: 8px 24px;
+    font-size: 16px;
+    line-height: 26px;
+    border: 0px;
+    border-radius:4px!important;
+    box-shadow: black!important;
+    box-shadow: 50px black;
+    cursor: pointer;
+    box-shadow: 0px 0px 15px rgba(255,151,79, 0.8);
+    font-family: 'Inter-SemiBold';
+}
+.primary:hover{
+    background: linear-gradient(93.62deg, #F2B430 0%, #F46E26 99.35%);
+    box-shadow: 0px 4px 4px rgba(244, 110, 38, 0.25);
+    border-radius: 4px;
+}
+.primary:active{
+    background: #E75E25;
+    border-radius: 4px;
+}
+
+</style>
