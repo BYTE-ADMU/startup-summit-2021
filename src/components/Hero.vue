9946d5dcf0080386b4f68a48af2f5a70014a10de
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2021-01-13 02:46:48 +0800

    added mobile (320px) responsiveness

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -12,7 +12,7 @@
                 <p id="date">April 16-18, 2020</p>
             </div>
             <div class="register-btn">
-                <button>Register ></button>
+                <button>Register ></button><button id="secondary-btn">View Schedule</button>
             </div>
         </div>
         <div class="messenger-btn">
@@ -55,8 +55,8 @@ export default {
     overflow: hidden;
 }
 .img-wrapper{
-    width: 954px!important;
-    height: 953px!important;
+    width: 954px;
+    height: 953px;
     transform: translate(-30%,-8%);
 }
 .img-wrapper img{
@@ -128,7 +128,9 @@ export default {
     width: 100%;
     height: 100%;
 }
-
+#secondary-btn{
+    visibility: hidden;
+}
 @media screen and (max-width: 768px){
 .page-container{
     display: flex;
@@ -137,16 +139,15 @@ export default {
 .img-wrapper{
     width: 825.64px;
     height: 824.78px;
-    transform: translate(-32%,-20%);
+    transform: translate(-30%,-20%);
 }
 .img-wrapper img{
     width: 100%;
     height: 100%;
-    z-index: 20;
-    transform: rotate(82deg);
+    transform: rotate(80deg);
 }
 .first-container{
-    transform: translate(38%,-85%);
+    transform: translate(38%,-73%);
 }
 .messenger-btn{
     transform: translate(0%,0%);
@@ -156,6 +157,52 @@ export default {
     width: 0px!important;
     height: 0px!important;
     visibility: hidden;
+}
+#secondary-btn{
+    visibility: visible;
+}
+
+@media screen and (max-width: 320px){
+.img-wrapper{
+    width: 473.05px;
+    height: 472.56px;
+    transform: translate(-30%,-44%);
+}
+.img-wrapper img{
+    transform: rotate(87deg);
+}
+.logo-wrapper{
+    width: 217px;
+    height: 55.05px;
+}
+.text-wrapper{
+    padding-top: 16px;
+}
+.text-wrapper #text{
+    font-family: "Objectivity-ExtraBold";
+    color: #2c2c2c;
+    font-size: 16px;
+    line-height: 17px;
+}
+.text-wrapper #date{
+    font-family: "Inter-SemiBold";
+    color: #A3A3A3;
+    padding-top: 16px;
+    font-size: 14px;
+    line-height: 22px;
+}
+.first-container{
+    transform: translate(6%,-73%);
+}
+.register-btn{
+    width: 151px;
+    display: flex;
+    flex-direction: column;
+}
+.register-btn button{
+    font-size: 14px;
+    line-height: 22px;
+}
 }
 }
 </style>
