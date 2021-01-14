a548a21175dfe354d86897313f9b72ee3598cacb
GDivino <68019100+GDivino@users.noreply.github.com>
2021-01-14 15:32:28 +0800

    Card component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Cards/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cards from './components/Cards.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cards
   }
 }
 </script>
@@ -19,8 +18,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
