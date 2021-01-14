84fd02911e175a0a06c0d6c1604a1e529e85254c
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2021-01-15 03:04:28 +0800

    started adding elements on startup page

--- src/components/Startup.vue
+++ src/components/Startup.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="page-container">
+        <center><div class="first-container">
+            <div class="big-img-wrapper">
+                <center><img id="main-img" src="#"></center>
+                <img class="circle-design" src="@/assets/circledesign.png">
+            </div>
+            </div>
+        </center>
     </div>
 </template>
 
@@ -32,11 +39,25 @@ export default {
 }
 .page-container{
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+    flex-direction: column;
     width: 100vw;
     margin: 0px!important;
-    height: 100vh;
     overflow-x: hidden;
+}
+.first-container{
+    height: 100vh;
+    padding-top: 5%;
+}
+.big-img-wrapper{
+    background-color: #C4C4C4;
+    width: 906px;
+    height: 510px;
+    position: relative;
+    z-index: 10!important;
+}
+.circle-design{
+    position: relative;
+    z-index: 0!important;
+    transform: translate(0%,0%);
 }
 </style>
