c6908de58ac7d6c7c239b815563ca17734400204
clarzcastillo <clarizza.castillo@obf.ateneo.edu>
2021-01-24 17:14:06 +0800

    added fonts and page cover

---
+++ src/components/StartupTemplate.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="page-container">
+        <div class="cover-wrapper"><img src="@/assets/template_cover.png"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Startup-Template'
+}
+</script>
+
+<style scoped>
+@font-face {
+  font-family: "Objectivity-ExtraBold";
+  src: url('./../fonts/Objectivity-ExtraBold.otf') format("opentype");
+}
+@font-face {
+  font-family: "Inter-Bold";
+  src: url('./../fonts/Inter-Bold.ttf') format("truetype");
+}
+@font-face {
+  font-family: "Inter-SemiBold";
+  src: url('./../fonts/Inter-SemiBold.ttf') format("truetype");
+}
+@font-face {
+  font-family: "Inter-Regular";
+  src: url('./../fonts/Inter-Regular.ttf') format("truetype");
+}
+@font-face {
+  font-family: "Inter-Light";
+  src: url('./../fonts/Inter-Light.ttf') format("truetype");
+}
+*{
+    background-color: #fcfcfc;
+    margin: 0px!important;
+}
+.page-container{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    margin: 0px!important;
+    display: flex;
+    flex-direction: column;
+    overflow-x: hidden;
+}
+.cover-wrapper{
+width: 100vw!important;
+height: 120px;
+}
+.cover-wrapper img{
+width: 100%;
+height: 100%;
+}
+</style>
