b52e83e229fdea299cab3645b7a0bb86ebea3283
GDivino <68019100+GDivino@users.noreply.github.com>
2021-02-21 12:22:44 +0800

    Add strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '05b29a8887e769632ac127ee654e97fd'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4ba11b73-6f70-475c-b00c-dd1467444376'
+};
