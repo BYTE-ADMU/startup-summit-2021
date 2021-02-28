fbbb679c21b9b41ce1323f9d37ef782e59204452
Franz Taborlupa <franztaborlupa.tech@gmail.com>
2021-02-28 16:02:57 +0800

    Fix App.vue for Home.vue merge

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <template>
-<Hero />
-<WhatIsSus />
-<div data-aos="fade-up">
-  <Home />
-</div>
-  <Opportunities />
-<Partners />
 <Navbar />
     <router-view v-slot="slotProps">
       <transition name="fade" mode="out-in">
@@ -15,22 +8,12 @@
     <Footer />
 </template>
 <script>
-import Hero from "@/components/Hero.vue"
-import Opportunities from "@/components/Opportunities.vue"
-import Partners from "@/components/Partners.vue"
-import Home from "./components/Home.vue"
-import WhatIsSus from "@/components/WhatIsSus.vue"
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 
 export default {
   name: "App",
   components: {
-    Home,
-    WhatIsSus,
-    Partners,
-    Hero,
-    Opportunities,
     Navbar,
     Footer
   }
