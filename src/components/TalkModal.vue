1e7393ae84613811f483c9db0074c7c4919f6301
GDivino <68019100+GDivino@users.noreply.github.com>
2021-03-21 10:28:06 +0800

    Fix exit button

--- src/components/TalkModal.vue
+++ src/components/TalkModal.vue
@@ -266,12 +266,12 @@ export default {
     width: 5px;
 }
 .talk_details .description::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+    box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
     border-radius: 10px;
 }
 .talk_details .description::-webkit-scrollbar-thumb {
     border-radius: 10px;
-    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5);
+    box-shadow: inset 0 0 6px rgba(0,0,0,0.5);
 }
 .talk_details .watch_now {
     width: 162px;
