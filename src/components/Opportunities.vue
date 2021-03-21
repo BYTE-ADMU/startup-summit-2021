d65d76e5cece63288006ce9325c4decbb1bd336d
GDivino <68019100+GDivino@users.noreply.github.com>
2021-03-21 10:34:59 +0800

    Fix home links

--- src/components/Opportunities.vue
+++ src/components/Opportunities.vue
@@ -34,7 +34,7 @@
                 <p>...AND MORE!</p>
             </div>
             <div class="view-btn">
-                <span class="primary-btn"><PrimaryBtn>View featured startups<Chevron id="chevron"/></PrimaryBtn></span>
+                <router-link to="/startups" class="primary-btn"><PrimaryBtn>View featured startups<Chevron id="chevron"/></PrimaryBtn></router-link>
                 <span class="primary-btn2"><PrimaryBtn>View startups<Chevron id="chevron"/></PrimaryBtn></span>
             </div>
         </div>
